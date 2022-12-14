788d5d8facc82b531f0e6a86e58cdb69bc85441d
SD-IITKGP <jaihindsourabh@kgpian.iitkgp.ac.in>
2022-12-14 09:51:45 +0530

    Added facebook login and Completed the project

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 const session =require("express-session");
 const passport = require("passport");
 const passpoerLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findorCreate = require("mongoose-findorcreate");
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
 app.use(bodyParser.urlencoded({extended:true}));
@@ -24,15 +27,55 @@ mongoose.connect("mongodb://localhost:27017/UsersDB");
 const userSchema = new mongoose.Schema({
   username:String,
   password:String,
+  googleId:String,
+  facebookId:String,
+  secret:String
 });
 userSchema.plugin(passpoerLocalMongoose);
+userSchema.plugin(findorCreate);
 // userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 const User = mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL: "https://www.googleapis.com/oauth2/v/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 app.get("/",function(req,res){
-  res.render("home",{});
+  if(req.isAuthenticated()){
+    res.redirect("/secrets");
+  }
+  else{
+    res.render("home",{});
+  }
 });
 app.get("/register",function(req,res){
   res.render("register",{});
@@ -40,17 +83,50 @@ app.get("/register",function(req,res){
 // app.get("/logout",function(req,res){
 //   res.redirect("/");
 // });
+
 app.get("/logout",function(req,res,next){
   req.logout(function(err) {
     if (err) { return next(err); }
     res.redirect('/');
   });
 });
+app.get('/auth/google',passport.authenticate('google', { scope: ['profile'] }));
+app.get('/auth/google/secrets',passport.authenticate('google', { failureRedirect: '/login' }),function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/secrets');
+});
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 app.get("/login",function(req,res){
   res.render("login",{});
 });
 app.get("/submit",function(req,res){
-  res.render("submit",{});
+  if(req.isAuthenticated()){
+    res.render("submit",{});
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+app.post("/submit",function(req,res){
+  User.findById(req.user.id,function(err,foundUser){
+    if(err){
+      console.log(err);
+      res.redirect("/submit");
+    }
+    else if(foundUser){
+      foundUser.secret=req.body.secret;
+      foundUser.save();
+      res.redirect("/secrets");
+    }
+  })
 });
 // app.post("/register",function(req,res){
 //   bcrypt.hash(req.body.password,saltRounds,function(err,hash){
@@ -68,7 +144,15 @@ app.get("/submit",function(req,res){
 // });
 app.get("/secrets",function(req,res){
   if(req.isAuthenticated()){
-    res.render("secrets",{});
+    User.find({"secret":{$ne:null}},function(err,foundUsers){
+      if(err){
+        console.log(err);
+        res.redirect("/");
+      }
+      else{
+          res.render("secrets",{foundUsers:foundUsers});
+      }
+    });
   }
   else{
     res.redirect("/login");
@@ -134,3 +218,27 @@ app.post("/login",function(req,res){
 app.listen(3000,function(){
   console.log("Server is running on port 3000");
 });
+
+
+
+// <script>
+//   window.fbAsyncInit = function() {
+//     FB.init({
+//       appId      : '{your-app-id}',
+//       cookie     : true,
+//       xfbml      : true,
+//       version    : '{api-version}'
+//     });
+//
+//     FB.AppEvents.logPageView();
+//
+//   };
+//
+//   (function(d, s, id){
+//      var js, fjs = d.getElementsByTagName(s)[0];
+//      if (d.getElementById(id)) {return;}
+//      js = d.createElement(s); js.id = id;
+//      js.src = "https://connect.facebook.net/en_US/sdk.js";
+//      fjs.parentNode.insertBefore(js, fjs);
+//    }(document, 'script', 'facebook-jssdk'));
+// </script>
